e996ff938ed52baa91840a7e02e41e45fa633f6d
pcfazzioni <carolina.sisdev@hotmail.com>
2026-09-03 18:45:44 -0300

    Adiciona perfis de vagas e atualiza candidatos

--- skillmatch.js
+++ skillmatch.js
@@ -10,14 +10,51 @@ const candidatos = [
   {
     nome: "Carolina Minuzzi Parmagnani",
     idade: 34,
-    experiencia: 1,
-    habilidades: ["HTML", "CSS", "JavaScript"]
+    areaDeInteresse: ["Desenvolvedor Front-End Júnior"],
+    habilidades: ["HTML", "CSS", "JavaScript", "Kanban", "Inglês"],
+    anosDeExperiência: 1,
   },
   //Candidato 2
   {
-    nome: "Carlos Eduardo da Silva",
+    nome: "Carlos Eduardo Silva",
     idade: 42,
-    experiencia: 4,
-    habilidades: ["HTML", "CSS", "JavaScript", "React"]
+    areaDeInteresse: ["Desenvolvedor Front-End Júnior", "Desenvolvedor Full-Stack"],
+    habilidades: ["HTML", "CSS", "JavaScript", "TypeScript", "React", "Kanban", "Inglês"],
+    anosDeExperiência: 4,
+  }
+];
+//Vagas Disponíveis
+const vagas = [
+  //Vaga 1
+  {
+    empresa: "WebFusion",
+    cargo: "Desenvolvedor Front-End Júnior",
+    requisitos: ["HTML", "CSS", "JavaScript", "TypeScript", "React", "Kanban", "Inglês"],
+    modalidade: "Remoto"
+    salário: 6000,
+  },
+  //Vaga 2
+  {
+    empresa: "ACSHealth"
+    cargo: "Desenvolvedor Front-End Sênior",
+    requisitos: ["HTML5","CSS","JavaScript","React","Redux Toolkit", "API/Rest", "Git", "TDD", "Testes automatizados" "Responsividade", "Figma", "SQL", "Docker", "Tailwind CSS", "AWS", "CloudFrond", "Arquitetura de Software", "Code Review", "Segurança de Aplicações", "Scrum", "Kanban"],
+    modalidade: "Semi-presencial"
+    salário: 15000,
+  },
+  //Vaga 3
+  {
+    empresa: "InovaTech"
+    cargo: "Desenvolvedor Front-End Júnior"
+    requisitos: ["HTML","CSS","JavaScript","React","TDD/Testes","API/Rest", "Git"],
+    modalidade: "Remoto"
+    salário: 10000,
+  },
+  //Vaga 4
+  {
+    empresa: "BaironDev"
+    cargo: "Desenvolvedor Front-End Júnior"
+    requisitos: ["HTML", "CSS", "JavaScript", "Kanban"],
+    modalidade: "Presencial"
+    salário: 5000,
   }
 ];
