1f20afeb983465545dd959081faa4dfeb4ddb993
carolinafazzioni <carolina.sisdev@hotmail.com>
2026-09-04 19:58:34 -0300

    Corrige sintaxe das vagas

--- skillmatch.js
+++ skillmatch.js
@@ -30,31 +30,31 @@ const vagas = [
     empresa: "WebFusion",
     cargo: "Desenvolvedor Front-End Júnior",
     requisitos: ["HTML", "CSS", "JavaScript", "TypeScript", "React", "Kanban", "Inglês"],
-    modalidade: "Remoto"
+    modalidade: "Remoto",
     salário: 6000,
   },
   //Vaga 2
   {
-    empresa: "ACSHealth"
+    empresa: "ACSHealth",
     cargo: "Desenvolvedor Front-End Sênior",
-    requisitos: ["HTML5","CSS","JavaScript","React","Redux Toolkit", "API/Rest", "Git", "TDD", "Testes automatizados" "Responsividade", "Figma", "SQL", "Docker", "Tailwind CSS", "AWS", "CloudFrond", "Arquitetura de Software", "Code Review", "Segurança de Aplicações", "Scrum", "Kanban"],
-    modalidade: "Semi-presencial"
+    requisitos: ["HTML5", "CSS", "JavaScript","React","Redux Toolkit", "API/Rest", "Git", "TDD", "Testes automatizados", "Responsividade", "Figma", "SQL", "Docker", "Tailwind CSS", "AWS", "CloudFrond", "Arquitetura de Software", "Code Review", "Segurança de Aplicações", "Scrum", "Kanban"],
+    modalidade: "Semi-presencial",
     salário: 15000,
   },
   //Vaga 3
   {
-    empresa: "InovaTech"
-    cargo: "Desenvolvedor Front-End Júnior"
+    empresa: "InovaTech",
+    cargo: "Desenvolvedor Front-End Júnior",
     requisitos: ["HTML","CSS","JavaScript","React","TDD/Testes","API/Rest", "Git"],
-    modalidade: "Remoto"
+    modalidade: "Remoto",
     salário: 10000,
   },
   //Vaga 4
   {
-    empresa: "BaironDev"
-    cargo: "Desenvolvedor Front-End Júnior"
+    empresa: "BaironDev",
+    cargo: "Desenvolvedor Front-End Júnior",
     requisitos: ["HTML", "CSS", "JavaScript", "Kanban"],
-    modalidade: "Presencial"
+    modalidade: "Presencial",
     salário: 5000,
   }
 ];
